809afe820013de4722c86925bfd019d55cbb1aa7
schackv <schackv@users.noreply.github.com>
2014-09-02 13:05:34 +0200

    Changed data backend to file and pickle

--- willie/modules/kaffe.py
+++ willie/modules/kaffe.py
@@ -9,11 +9,13 @@ Created on Mon Sep  1 21:36:31 2014
 
 import willie
 import datetime
+import pickle
+DATA_FILE = 'botdata.p'
 
-def setup(bot):
+#def setup(bot):
     #Having a db means pref's exists. Later, we can just use `if bot.db`.
-    if bot.db and not bot.db.preferences.has_columns('coffeecount'):
-        bot.db.preferences.add_columns(['coffeecount'])
+    #if bot.db and not bot.db.preferences.has_columns('coffeecount'):
+        #bot.db.preferences.add_columns(['coffeecount'])
         
         
 def configure(config):
@@ -30,6 +32,7 @@ def kaffe(bot, trigger):
         bot.LAST_COFFEE_CALL = datetime.datetime.min
     
     print(bot.LAST_COFFEE_CALL)
+    
     # Check whether time enough has been spent
     kaffedelta = datetime.timedelta(0,0,0,0,float(bot.config.kaffe.coffee_delta))
     ddiff = datetime.datetime.now()-bot.LAST_COFFEE_CALL
@@ -37,34 +40,25 @@ def kaffe(bot, trigger):
         bot.say('\00303Ikke kaffe igen allerede, {}. Der er kun gået {} siden sidst.'.format(caller_nick,ddiff))
     else:   # Allow coffee call
         bot.LAST_COFFEE_CALL = datetime.datetime.now()
-        s = ''
-        if bot.db:
-            count = increase_coffee_count(bot.db,trigger.nick)
-            s = ' ({}. kald)'.format(count)
-        else:
-            s = 'dberr'
         
+        count = increase_coffee_count(trigger.nick)
+        s = ' ({}. kald)'.format(count)
+
         bot.say('\037\00304{} indkalder til kaffe.{}'.format(caller_nick,s))
         bot.say('\00303{}'.format(' '.join(nicks)))
 
 @willie.module.commands('tally')
 def tally(bot,trigger):
-    if not bot.db:
-        bot.say('Database not configured.')
-        return
     
-    count = total_count(bot.db)
+    count = total_count()
     caffeine_per_cup = float(bot.config.kaffe.caffeine_per_cup)
-    bot.say('\037\00304{} fælleskopper nået (cirka {}g ren koffein).'.format(count,count*caffeine_per_cup))
+    bot.say('\037\00304{} fælleskopper nået (cirka {:.4f}g ren koffein).'.format(count,count*caffeine_per_cup))
         
 @willie.module.commands('top5')
 def top5(bot,trigger):
-    if not bot.db:
-        bot.say('Database not configured.')
-        return
 
     # Get a sorted list of tuples (descending)
-    x = individual_counts(bot.db)
+    x = individual_counts()
     sorted_x = sorted(x, key=lambda tup: tup[1],reverse=True)
     
     # Cut to 5
@@ -86,33 +80,34 @@ def frokost(bot,trigger):
     bot.say('\00303{0}'.format(' '.join(nicks)))
     
 
-def coffee_count(db,nick):
-    if nick in db.preferences:
-        return int(db.preferences.get(nick, 'coffeecount'))
-    else:
-        return 0
+def coffee_count(nick):
+    return get_value(nick,'coffeecount',0)
+    
 
-def total_count(db):
+def total_count():
     total = 0
-    for elmnt in db.preferences.keys('coffeecount'):
-        total += int(elmnt[0])
+    for usr, count in individual_counts():
+        total += count
     return total
 
 
-def individual_counts(db):
-    users = [key[0] for key in db.preferences.keys()]
-    
+def individual_counts():
+    userlist = read_userlist()
+
     counts = []
-    for usr in users:
-        counts.append( (usr,coffee_count(db,usr)) )
+    for usr in userlist:
+        print(usr)
+        counts.append( (usr,userlist[usr]['coffeecount']))
         
     return counts
 
     
-def increase_coffee_count(db,nick):
-    old_count = coffee_count(db,nick)
+def increase_coffee_count(nick):
+    old_count = get_value(nick,'coffeecount',0)
     new_count = old_count+1
-    db.preferences.update(nick, {'coffeecount': '{}'.format(new_count)})
+    #db.preferences.update(nick, {'coffeecount': '{}'.format(new_count)})
+    set_value(nick,'coffeecount',new_count)
+
     return new_count
     
 def decompose_info(bot,trigger):
@@ -124,7 +119,33 @@ def decompose_info(bot,trigger):
 def privs2nicks(bot,channel):
     return ['{}'.format(key) for key in bot.privileges[channel].keys()]
     
+
+#### Data I/O
     
-def last_time(db):
-    return db.preferences.get()
+def get_value(nick,key,default_value=None):
+    # Read a value of a given variable for a given user
+    userlist = read_userlist()
+    if nick in userlist:
+        if key in userlist[nick]:
+            return userlist[nick][key]
+    return default_value
+
+def set_value(nick,key,value):
+    userlist = read_userlist()
+    if not nick in userlist:
+        userlist[nick] = {}
+    userlist[nick][key] = value
+    write_userlist(userlist)
+
+def read_userlist():
+    import os
+    if os.path.isfile(DATA_FILE):
+        with open(DATA_FILE,'rb') as f:
+            usrlist = pickle.load(f)
+        return usrlist
+    else:
+        return {}
     
+def write_userlist(userlist):    
+    with open(DATA_FILE,'wb') as f:
+        pickle.dump(userlist,f)
