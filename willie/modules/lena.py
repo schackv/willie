dca67d072443a9425857162be1db2e8bb411f9e3
schackv <schackv@users.noreply.github.com>
2014-09-02 15:23:00 +0200

    added lena module

---
+++ willie/modules/lena.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  1 21:36:31 2014
+
+@author: jsve
+"""
+
+import willie
+
+
+@willie.module.commands('lena')
+def lena(bot, trigger):
+    bot.say('http://ic.pics.livejournal.com/alexey47/23384793/1620040/1620040_original.jpg')
